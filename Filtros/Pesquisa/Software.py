ae4f6a785469e5b3126cb86f231e9589d0f2c55c
Alice_Galvao <124831712+AliceGalvao@users.noreply.github.com>
2025-10-09 14:37:58 -0300

    Add files via upload

---
+++ Filtros/Pesquisa/Software.py
@@ -0,0 +1,10 @@
+from Filtros.constantes_filtros import *
+
+class Software:
+
+    def __init__(self, tipo, titulo, codigo, instituicao_deposito_registro, data_concessao):
+        self.tipo = tipo
+        self.titulo = titulo
+        self.codigo = codigo
+        self.instituicao_deposito_registro = instituicao_deposito_registro
+        self.data_concessao = data_concessao
