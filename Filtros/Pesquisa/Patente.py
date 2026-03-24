38b06d6e836fddda770f912c86ae836355de3a20
Alice_Galvao <124831712+AliceGalvao@users.noreply.github.com>
2026-03-24 14:11:00 -0300

    Correção para que o histograma funcione Patente.py

--- Filtros/Pesquisa/Patente.py
+++ Filtros/Pesquisa/Patente.py
@@ -2,10 +2,10 @@ from Filtros.constantes_filtros import *
 
 class Patente:
 
-    def __init__(self, tipo, titulo, codigo, instituicao_deposito_registro, data_concessao):
+    def __init__(self, tipo, titulo, codigo, instituicao_deposito_registro, ano):
         
         self.tipo = tipo
         self.titulo = titulo
         self.codigo = codigo
         self.instituicao_deposito_registro = instituicao_deposito_registro
-        self.data_concessao = data_concessao
+        self.ano = str(ano)[-4:]
