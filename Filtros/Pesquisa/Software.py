8c25d3dab197a188f8a08590045a4a21a10ee60a
Alice_Galvao <124831712+AliceGalvao@users.noreply.github.com>
2026-03-24 14:11:34 -0300

    Correção para que o histograma funcione Software.py

--- Filtros/Pesquisa/Software.py
+++ Filtros/Pesquisa/Software.py
@@ -2,9 +2,9 @@ from Filtros.constantes_filtros import *
 
 class Software:
 
-    def __init__(self, tipo, titulo, codigo, instituicao_deposito_registro, data_concessao):
+    def __init__(self, tipo, titulo, codigo, instituicao_deposito_registro, ano):
         self.tipo = tipo
         self.titulo = titulo
         self.codigo = codigo
         self.instituicao_deposito_registro = instituicao_deposito_registro
-        self.data_concessao = data_concessao
+        self.ano = str(ano)[-4:]
